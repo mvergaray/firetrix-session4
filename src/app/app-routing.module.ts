15307cb43189d73af2c452fee7d90e557e4e8853
Luis del Aguila <luandaja94@gmail.com>
2018-05-04 16:49:38 -0500

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { UserLoginComponent } from './ui/user-login/user-login.component';
+import { ItemsListComponent } from './items/items-list/items-list.component';
+import { ReadmePageComponent } from './ui/readme-page/readme-page.component';
+
+import { AuthGuard } from './core/auth.guard';
+import { CoreModule } from './core/core.module';
+
+const routes: Routes = [
+  { path: '', component: ReadmePageComponent },
+  { path: 'login', component: UserLoginComponent },
+  { path: 'items', component: ItemsListComponent, canActivate: [AuthGuard] },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [AuthGuard],
+})
+export class AppRoutingModule { }
